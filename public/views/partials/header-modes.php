eeeb630fc446e71cfff88896dbddc0da326589a0
Araceli <gon.araceli.c@gmail.com>
2025-10-15 01:04:43 -0300

    modificaciones x

--- public/views/partials/header-modes.php
+++ public/views/partials/header-modes.php
@@ -1,12 +1,5 @@
 <button type="button" class="btn btn-system" data-action="toggle-theme" aria-label="Toggle Dark Mode" aria-pressed="false">
-<<<<<<< HEAD
-            <svg class="sa-icon sa-icon-2x">
-                <use href="/converseLarry/public/assets/img/sprite.svg#circle"></use>
-            </svg>
-        </button>
-=======
     <svg class="sa-icon sa-icon-2x">
         <use href="http://converse.local/assets/img/sprite.svg#circle"></use>
     </svg>
 </button>
->>>>>>> 61235dafd1a9220ad9e5d61799d036954e5c8f03
--- public/views/partials/header-notifications.php
+++ public/views/partials/header-notifications.php
@@ -1,14 +1,6 @@
 <button type="button" class="btn btn-system dropdown-toggle no-arrow" data-bs-toggle="dropdown" aria-expanded="false" aria-label="Open Notifications">
-<<<<<<< HEAD
-            <span class="badge badge-icon pos-top pos-end">5</span>
-            <svg class="sa-icon sa-icon-2x">
-                <use href="/converseLarry/public/assets/img/sprite.svg#bell"></use>
-            </svg>
-        </button>
-=======
     <span class="badge badge-icon pos-top pos-end">5</span>
     <svg class="sa-icon sa-icon-2x">
         <use href="http://converse.local/assets/img/sprite.svg#bell"></use>
     </svg>
 </button>
->>>>>>> 61235dafd1a9220ad9e5d61799d036954e5c8f03
